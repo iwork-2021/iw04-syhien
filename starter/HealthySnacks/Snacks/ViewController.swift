e3ce0ba600ca99e15a4605095cb529097733e8c2
syhien <syhien@outlook.at>
2021-11-20 18:42:40 +0800

    成功实现了识别最可能的snack

--- starter/HealthySnacks/Snacks/ViewController.swift
+++ starter/HealthySnacks/Snacks/ViewController.swift
@@ -27,6 +27,8 @@
 /// THE SOFTWARE.
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
     
@@ -38,6 +40,21 @@ class ViewController: UIViewController {
     @IBOutlet var resultsConstraint: NSLayoutConstraint!
     
     var firstTime = true
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do{
+            let classifier = try SnacksClassifier(configuration: MLModelConfiguration())
+            let model = try VNCoreMLModel(for: classifier.model)
+            let request = VNCoreMLRequest(model: model, completionHandler: {
+                [weak self] request,error in
+                self?.processObservations(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to create request")
+        }
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -96,6 +113,14 @@ class ViewController: UIViewController {
     }
     
     func classify(image: UIImage) {
+        let scaledImage = image.scalePreservingAspectRatio(targetSize: CGSize(width: 299, height: 299))
+        guard let pixelbuffer = scaledImage.toCVPixelBuffer() else { return }
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelbuffer)
+        do {
+            try handler.perform([self.classificationRequest])
+        } catch {
+            print("Failed to perform classification: \(error)")
+        }
     }
 }
 
@@ -107,5 +132,86 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         imageView.image = image
         
         classify(image: image)
+    }
+}
+
+extension ViewController {
+    func processObservations(for request: VNRequest, error: Error?) {
+        if let results = request.results as? [VNClassificationObservation] {
+            if results.isEmpty {
+                self.resultsLabel.text = "Nothing found"
+            } else {
+                let result = results[0].identifier
+                let confidence = results[0].confidence
+                self.resultsLabel.text = result
+                self.resultsLabel.text! += "\n"
+                self.resultsLabel.text! += String(format: "%.1f%%", confidence * 100)
+                print(result)
+            }
+        } else if let error = error {
+            self.resultsLabel.text = "Error: \(error.localizedDescription)"
+        } else {
+            self.resultsLabel.text = "???"
+        }
+        self.showResultsView()
+    }
+}
+
+extension UIImage {
+    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
+            // Determine the scale factor that preserves aspect ratio
+            let widthRatio = targetSize.width / size.width
+            let heightRatio = targetSize.height / size.height
+            
+            let scaleFactor = min(widthRatio, heightRatio)
+            
+            // Compute the new image size that preserves aspect ratio
+            let scaledImageSize = CGSize(
+                width: size.width * scaleFactor,
+                height: size.height * scaleFactor
+            )
+
+            // Draw and return the resized UIImage
+            let renderer = UIGraphicsImageRenderer(
+                size: scaledImageSize
+            )
+
+            let scaledImage = renderer.image { _ in
+                self.draw(in: CGRect(
+                    origin: .zero,
+                    size: scaledImageSize
+                ))
+            }
+            
+            return scaledImage
+        }
+    
+    func toCVPixelBuffer() -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard status == kCVReturnSuccess else {
+            return nil
+        }
+
+        if let pixelBuffer = pixelBuffer {
+            CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+            let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
+
+            let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+            let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+            context?.translateBy(x: 0, y: self.size.height)
+            context?.scaleBy(x: 1.0, y: -1.0)
+
+            UIGraphicsPushContext(context!)
+            self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+            UIGraphicsPopContext()
+            CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+
+            return pixelBuffer
+        }
+
+        return nil
     }
 }
